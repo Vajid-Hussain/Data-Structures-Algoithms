6bb5698eb6d80598fef55d6576016307941ee0f7
Vajid-Hussain <vajidhussain17@gmail.com>
2024-07-09 09:34:24 +0530

    /2

--- linkedList/singleLinkedList/2.go
+++ linkedList/singleLinkedList/2.go
@@ -38,3 +38,56 @@
 
 //     return head.Next
 // }
+
+
+
+
+//  func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+// 	var (
+// 		balance int
+// 		result  = l1
+// 	)
+
+// 	for l1 != nil && l2 != nil {
+// 		temp := l1.Val
+// 		l1.Val = ((l1.Val+l2.Val) + balance) %10
+// 		// balance = -1
+// 		balance = (temp + l2.Val + balance) / 10
+
+// 		// fmt.Println(balance, (temp + l2.Val) , l1.Val)
+
+// 		if l1.Next == nil && l2.Next == nil {
+//             if balance >0{
+//                 // for balance >0{
+//                     l1.Next = &ListNode{Val:balance}
+//                     balance = -1
+//                 // }
+//                 break
+//             }
+// 			l1 = l1.Next
+// 			l2 = l2.Next
+// 			continue
+// 		} else if l1.Next == nil {
+// 			l1.Next = l2.Next
+//             // fmt.Println(l1.Next, l2.Next)
+// 			l1=l1.Next
+//             break
+// 		}
+// 		l1 = l1.Next
+// 		l2 = l2.Next
+// 	}
+
+// 	for balance > 0 {
+// 		temp := l1.Val
+// 		l1.Val = (l1.Val + balance) % 10
+// 		balance = (temp + balance) / 10
+
+//         if balance >0 && l1.Next == nil{
+//             l1.Next = &ListNode{}
+//         }
+
+// 		l1 = l1.Next
+// 	}
+
+// 	return result
+// }
