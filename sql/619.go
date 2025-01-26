0a6c2be2591544e6b4eb0e8fd0f9525fbcc8e044
Vajid Hussain K <vajidhussain77@gmail.com>
2025-01-26 12:34:02 +0530

    Revert "619"
    This reverts commit aa5de53f5e521a839f311fa207d8b9f0ad351634.

--- sql/619.go
+++
@@ -1,8 +0,0 @@
-// # Write your MySQL query statement below
-// WITH helper AS (
-//     SELECT num FROM mynumbers
-//     GROUP BY num
-//     HAVING COUNT(*) = 1
-// )
-
-// SELECT MAX(num) AS num FROM helper
